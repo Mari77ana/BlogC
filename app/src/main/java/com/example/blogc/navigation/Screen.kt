cc29fed621213eee6cd425ab3670c2a29a6df7cc
m.laic <m.laic@hotmail.com>
2024-05-10 10:43:03 +0200

    added UserListScreen in Screen for prepering for navigation

--- app/src/main/java/com/example/blogc/navigation/Screen.kt
+++ app/src/main/java/com/example/blogc/navigation/Screen.kt
@@ -3,4 +3,6 @@ package com.example.blogc.navigation
 sealed class Screen (val route: String) {
     object FormScreen: Screen("form_screen")
     object ProfileScreen: Screen("profile_screen")
+
+    object UserListScreen: Screen("userList-screen")
 }
