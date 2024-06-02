94b12c932bb8605b67a38304cdbbcdc599dd659a
m.laic <m.laic@hotmail.com>
2024-06-02 22:16:40 +0200

    Sending just to incase

--- app/src/main/java/com/example/blogc/MainActivity.kt
+++ app/src/main/java/com/example/blogc/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : ComponentActivity() {
                    )
 
                      */
-                     
+
                     Navigation()
                 }
             }
